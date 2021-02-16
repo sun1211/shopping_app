6ec50fba9f67524f570ada4662c0f56863fd1ce6
tamhoang <ngoctam121191@yahoo.com>
2021-02-16 11:24:09 +0700

    missing ;

--- lib/screens/details/components/app_bar.dart
+++ lib/screens/details/components/app_bar.dart
@@ -3,27 +3,27 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shopping_app/constants.dart';
 import 'package:shopping_app/models/Product.dart';
 
-AppBar DetailAppBar(BuildContext context, Product product){
+AppBar DetailAppBar(BuildContext context, Product product) {
   return AppBar(
-          backgroundColor: product.color,
-      elevation: 0,
-      leading: IconButton(
-        icon: SvgPicture.asset(
-          'assets/icons/back.svg',
-          color: Colors.white,
-        ),
-        onPressed: () => Navigator.pop(context),
+    backgroundColor: product.color,
+    elevation: 0,
+    leading: IconButton(
+      icon: SvgPicture.asset(
+        'assets/icons/back.svg',
+        color: Colors.white,
       ),
-      actions: <Widget>[
-        IconButton(
-          icon: SvgPicture.asset("assets/icons/search.svg"),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: SvgPicture.asset("assets/icons/cart.svg"),
-          onPressed: () {},
-        ),
-        SizedBox(width: kDefaultPaddin / 2)
-      ],
-  )
+      onPressed: () => Navigator.pop(context),
+    ),
+    actions: <Widget>[
+      IconButton(
+        icon: SvgPicture.asset("assets/icons/search.svg"),
+        onPressed: () {},
+      ),
+      IconButton(
+        icon: SvgPicture.asset("assets/icons/cart.svg"),
+        onPressed: () {},
+      ),
+      SizedBox(width: kDefaultPaddin / 2)
+    ],
+  );
 }
